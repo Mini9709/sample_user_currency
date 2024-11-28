5da292a9463ef9c6ab6070c309d4390304711269
Mini9709 <tjdals9709@naver.com>
2024-11-28 18:20:07 +0900

    feat : @NotNull 어노테이션 추가

--- src/main/java/com/sparta/currency_user/dto/CurrencyRequestDto.java
+++ src/main/java/com/sparta/currency_user/dto/CurrencyRequestDto.java
@@ -1,10 +1,7 @@
 package com.sparta.currency_user.dto;
 
 import com.sparta.currency_user.entity.Currency;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 import java.math.BigDecimal;
@@ -15,8 +12,13 @@ public class CurrencyRequestDto {
     @NotBlank
     private String currencyName;
 
+    @NotNull
     @Positive
     private BigDecimal exchangeRate;
+
+    @NotNull
+    @Positive
+    private BigDecimal baseCurrency;
 
     @Size(min = 1, max = 1, message = "올바른 기호를 입력해주세요.")
     @Pattern(regexp = "[₩$€¥£元₱₣]", message = "올바른 기호를 입력해주세요.")
@@ -26,7 +28,8 @@ public class CurrencyRequestDto {
         return new Currency(
                 this.currencyName,
                 this.exchangeRate,
-                this.symbol
+                this.symbol,
+                this.baseCurrency
         );
     }
 }
